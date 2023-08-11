1ad995bb1d78f7431c866e0899c3304cad575103
Hamza <hamza.eldefrawy4@gmail.com>
2023-08-11 16:10:25 -0700

    done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main -Entry point 
+ * main -Entry point
  *
  * Description: A C program that prinst with put function
  *
@@ -12,4 +12,4 @@ int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-} 
+}
