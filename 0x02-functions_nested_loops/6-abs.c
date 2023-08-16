ba505822053d61fca22782ca9945b5902353fefe
Hamza <hamza.eldefrawy4@gmail.com>
2023-08-15 18:40:14 -0700

    done

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include"mani.h"
+
+/**
+ * _abs - function that computes the absolute value of an integer
+ *
+ * @c: input of the function
+ *
+ * Return: always 0
+*/
+
+int _abs(int c)
+{
+	if (n < 0)
+		n = (-1) * n;
+	return (n);
+}
